aa5c2e723ccb554e162e96d7e347b09a6bcd2722
Ansh Kumar Singh <148905627+AnshKumar1936@users.noreply.github.com>
2024-02-16 15:19:30 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,13 @@
+dict={
+    "name" : "Ansh kumar singh",
+    "course" : "BCA", 
+    "section" : "C1"
+}
+
+print(dict)
+print(dict["course"])
+
+dict["name"]="Anmol"
+dict["surname"]="kumar"
+print(dict["name"])
+print(dict)
