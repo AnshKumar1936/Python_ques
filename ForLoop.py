d1e0db53eafc67da9ee7f6cd13fb9789870f619c
Ansh Kumar Singh <148905627+AnshKumar1936@users.noreply.github.com>
2024-02-20 22:37:59 +0530

    Add files via upload

---
+++ ForLoop.py
@@ -0,0 +1,20 @@
+num=[1,2,3,4,5]
+nums=["apple","mango","orange","papaya","litchi"]
+
+for i in num:
+    print(i)
+
+for i in nums:
+    print(i)
+
+tup=(1,2,3,4,5)
+
+for i in tup:
+    print(i)
+
+name="ansh kumar singh"
+
+for char in name:
+    print(char)
+else:
+    print("ENDS")
