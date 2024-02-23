ffaef182bc1848f036575cc3345ef83a94e5934c
Ansh Kumar Singh <148905627+AnshKumar1936@users.noreply.github.com>
2024-02-23 13:41:16 +0530

    Add files via upload

---
+++ Function.py
@@ -0,0 +1,24 @@
+# def sum(a,b):
+#     s=a+b
+#     return s
+# print (sum(2,3))
+
+
+# def sum(a,b):
+#     s=a+b
+#     print(s)
+#     return s
+
+# print (sum(2,3))
+
+
+# def sum(a,b):
+#     return a+b
+
+# print (sum(2,3))\
+
+
+# def hello():
+#     print("HelloWorld")
+
+# hello()
