b3f6d110a08b70ab380e321e7067042f8cf8d87c
Ansh Kumar Singh <148905627+AnshKumar1936@users.noreply.github.com>
2024-02-24 22:04:03 +0530

    Add files via upload

---
+++ FunctionQues3.py
@@ -0,0 +1,7 @@
+cities=["delhi","mumbai","chennai","pune","noida"]
+batsman=["virat","rohit","bumrah","dhoni","Yuvraj","Raina"]
+def print_length(list):
+    print(len(list))
+
+print_length(cities)
+print_length(batsman)
