58c44a2e44607b5d51afd1020cb2e84b1da8eea0
Ansh Kumar Singh <148905627+AnshKumar1936@users.noreply.github.com>
2024-03-03 13:29:54 +0530

    Add files via upload

---
+++ FileHandlingQues5.py
@@ -0,0 +1,8 @@
+f=open("Python\FilePractise.txt" , "w")
+
+f.write("Hi everyone")
+f.write("\nwe are learning file I/O")
+f.write("\nusing java")
+f.write("\nI like programming in Java")
+
+f.close()
