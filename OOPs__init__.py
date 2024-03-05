3d256a8b6553cd35ffad34b9df484c7da61d6375
Ansh Kumar Singh <148905627+AnshKumar1936@users.noreply.github.com>
2024-03-05 08:07:36 +0530

    Add files via upload

---
+++ OOPs__init__.py
@@ -0,0 +1,14 @@
+class student:
+    def __init__(self ,name , marks):
+        self.name = name
+        self.marks= marks
+        print("Add new database...")
+
+s1=student("karan",88)
+print(s1.name, s1.marks)
+
+s2=student("Ansh", 88)
+print(s2.name, s2.marks)
+
+s3=student("Anmol",88)
+print(s3.name, s3.marks)
